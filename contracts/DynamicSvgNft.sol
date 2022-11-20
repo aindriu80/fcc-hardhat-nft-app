a4402a088ae5fa4087df180e063c0aea6b00ffae
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2022-11-20 16:41:03 +0000

    Dynamic SVG On-Chain NFT Part V - Creating an NFT Token URI on-Chain

--- contracts/DynamicSvgNft.sol
+++ contracts/DynamicSvgNft.sol
@@ -3,8 +3,8 @@
 
 pragma solidity ^0.8.7;
 
-import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
-import 'base64-sol/base64.sol';        
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "base64-sol/base64.sol";
 
 contract DynamicSvgNft is ERC721 {
     // mint
@@ -16,11 +16,11 @@ contract DynamicSvgNft is ERC721 {
     string private i_highImageURI;
     string private constant base64EncodedSvgPrefix = "data:image/svg+xml;base64,";
 
-    constructor(string memory lowSvg, string memory highSvg) ERC721('Dynamic SVG NFT', 'DSN') {
+    constructor(string memory lowSvg, string memory highSvg) ERC721("Dynamic SVG NFT", "DSN") {
         s_tokenCounter = 0;
     }
 
-    function svgToImageURI(string memory svg) public pure returns(string memory){
+    function svgToImageURI(string memory svg) public pure returns (string memory) {
         string memory svgBase64Encoded = Base64.encode(bytes(string(abi.encodePacked(svg))));
         return string(abi.encodePacked(base64EncodedSvgPrefix, svgBase64Encoded));
     }
@@ -28,5 +28,35 @@ contract DynamicSvgNft is ERC721 {
     function mintNft() public {
         _safeMint(msg.sender, s_tokenCounter);
         s_tokenCounter = s_tokenCounter + 1;
+    }
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "data:application/json;base64,";
+    }
+
+    function tokenURI(uint256 tokenId) public view override returns (string memory) {
+        require(_exists(tokenId), "URI Query for nonexistent token");
+        string memory imageURI = "hi!";
+
+        // data:image/svg+xml;base64,
+        // data:application/json;base64,
+        return
+            string(
+                abi.encodePacked(
+                    _baseURI(),
+                    Base64.encode(
+                        bytes(
+                            abi.encodePacked(
+                                '{"name":"',
+                                name(),
+                                '" "description":"An NFT that changes based on the Chainlink Feed", ',
+                                '"attributes": [{"trait_type": "coolness", "value":100}], "image":"',
+                                imageURI,
+                                '"}'
+                            )
+                        )
+                    )
+                )
+            );
     }
 }
